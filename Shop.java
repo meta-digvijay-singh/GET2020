a0004e6799597d37bf6b8dc0b95554903c5df49a
Digvijay Singh <digvijay.singh@metacube.com>
2020-01-11 21:38:32 +0530

    Updated Shoppin Cart with all the changes required

--- Shop.java
+++ Shop.java
@@ -1,142 +1,217 @@
 import java.util.Scanner;
 
-public class Shop
-{
-	public static void main(String args[])
-	{
-		int choice = 1; // Choice = 1 initially displays Instructions for the person.  
-		boolean isExit = false;
-		String person;
+public class Shop {
+	private static Scanner sc = new Scanner(System.in);
+	
+	/*
+	 * Displays Available items in stock with their prices.
+	 */
+	private static void displayStockItems() {
+		ShoppingCart.displayAvailableItems();
+	}
+	
+	/*
+	 * Allows user to enter only integer value.
+	 * @return : value entered by user.
+	 */
+	private static int getIntegerValueOnly() {
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter user or admin : ");
-		person = sc.next();
-		person = person.toLowerCase();
-		if ("user".equals(person))
-		{
-			String itemName;
-			int quantity;
-			ShoppingCart userCart = new ShoppingCart();
-			ShoppingCart.displayAvailableItems(); //Display Available items in store.
-			while(true)
-			{	
-				switch (choice)
-				{
-					case 1:
-						System.out.println("\nInstructions : Press below keys for operation");
-						System.out.println("1. To get Instructions.");
-						System.out.println("2. Add Item to the cart");
-						System.out.println("3. Update Item of the cart");
-						System.out.println("4. Remove Item from the cart");
-						System.out.println("5. Display Cart Items with quantity");
-						System.out.println("6. Display Bill");
-						System.out.println("7. Display Available Items in store.");
-						System.out.println("Press any other digit for exit\n");
-						break;
-							
-					case 2:
-						System.out.println("Enter Item Name : ");
-						itemName = sc.next();
-						System.out.println("Enter quantity : ");
-						quantity = sc.nextInt();
-						userCart.addItem(itemName, quantity);
-						break;
-						
-					case 3:
-						System.out.println("Enter Item Name : ");
-						itemName = sc.next();
-						System.out.println("Enter quantity : ");
-						quantity = sc.nextInt();
-						userCart.updateItem(itemName, quantity);
-						break;
-						
-					case 4:
-						System.out.println("Enter Item Name : ");
-						itemName = sc.next();
-						userCart.removeItem(itemName);
-						break;
-						
-					case 5:
-						userCart.displayCart();
-						break;
-						
-					case 6:
-						userCart.generateBill();
-						break;
-					
-					case 7:
-						ShoppingCart.displayAvailableItems();
-						break;
-						
-					default:
-						isExit = true;
-				}
-				if (isExit)
-				{
-					System.out.println("Thanks for your time.");
-					break;
-				}
-				System.out.println("Enter your choice : ");
-				choice = sc.nextInt();
-				
-			}
-			
-		} 
-		
-		else if("admin".equals(person))
-		{
-			String itemName;
-			int price;
-			while(true)
-			{
-				switch(choice)
-				{
-					case 1:
-						System.out.println("Instructions : press below keys for operation");
-						System.out.println("1. To get Instructions.");
-						System.out.println("2. Add item to the stock.");
-						System.out.println("3. Remove item from stock.");
-						System.out.println("4. Display Stock Items.");
-						System.out.println("Enter any other number for exit.");
-						break;
-					
-					case 2:
-						System.out.println("Enter item name : ");
-						itemName = sc.next();
-						System.out.println("Enter price of the item : ");
-						price = sc.nextInt();
-						itemName = itemName.toLowerCase();
-						ShoppingCart.addStockItem(itemName, price);
-						break;
-					
-					case 3:
-						System.out.println("Enter item name");
-						itemName = sc.next();
-						itemName = itemName.toLowerCase();
-						ShoppingCart.discardStockItem(itemName);
-						break;
-						
-					case 4:
-						ShoppingCart.displayAvailableItems();
-						break;
-					
-					default:
-						isExit = true;
-				}
-				if (isExit)
-				{
-					System.out.println("Thanks for your time.");
-					break;
-				}
-				System.out.println("Enter your choice : ");
-				choice = sc.nextInt();
+		while (true) {
+			try {
+				return sc.nextInt();
+			} catch (Exception ex) {
+				System.out.println("Enter integer value only :");
+				sc.next();
 			}
 		}
-		else
-		{
+	}
+	
+	/*
+	 * Takes itemName from user.
+	 * @return : item name provided by user.
+	 */
+	private static String getItemName() {
+		String itemName;
+		
+		System.out.println("Enter Item Name : ");
+		itemName = sc.next();
+		return itemName;
+	}
+	
+	/*
+	 * Takes quantity of the item from the user.
+	 * @return : quantity provided by user.
+	 */
+	private static int getQuantity() {
+		int quantity;
+		
+		System.out.println("Enter quantity : ");
+		quantity = getIntegerValueOnly();
+		
+		return quantity;
+		
+	}
+	
+	/*
+	 * Takes price of item from user.
+	 * @return : price provided by user.
+	 */
+	private static int getPrice() {
+		int price;
+		
+		System.out.println("Enter price of the item : ");
+		price = getIntegerValueOnly();
+		
+		return price;
+	}
+	
+	/*
+	 * Actions user can perform.
+	 */
+	private static void userActions() {
+		int choice = 1; //choice = 1 provides instructions to the user initially.
+		String itemName;
+		int quantity;
+		boolean isExit = false;
+		
+		ShoppingCart userCart = new ShoppingCart();
+		
+		while(true) {	
+			
+			switch (choice) {
+				
+				case 1:
+					System.out.println("\nInstructions : Press below keys for operation");
+					System.out.println("1. To get Instructions.");
+					System.out.println("2. Add Item to the cart");
+					System.out.println("3. Update Item of the cart");
+					System.out.println("4. Remove Item from the cart");
+					System.out.println("5. Display Cart Items with quantity");
+					System.out.println("6. Display Bill");
+					System.out.println("7. Display Available Items in store.");
+					System.out.println("Press any other digit for exit\n");
+					break;
+						
+				case 2:
+					itemName = getItemName();
+					quantity = getQuantity();
+					userCart.addItem(itemName, quantity);
+					break;
+					
+				case 3:
+					itemName = getItemName();
+					quantity = getQuantity();
+					userCart.updateItem(itemName, quantity);
+					break;
+					
+				case 4:
+					itemName = getItemName();
+					userCart.removeItem(itemName);
+					break;
+					
+				case 5:
+					userCart.displayCart();
+					break;
+					
+				case 6:
+					userCart.generateBill();
+					break;
+				
+				case 7:
+					displayStockItems();
+					break;
+					
+				default:
+					isExit = true;
+			}
+			
+			if (isExit) {
+				System.out.println("Thanks for your time.");
+				break;
+			}
+			
+			System.out.println("Enter your choice : ");
+			choice = getIntegerValueOnly();
+		}
+	}
+	
+	/*
+	 * Actions administrator of the store can perform.
+	 */
+	private static void adminActions() {
+		int choice = 1; //choice = 1 provides instructions to the administrator initially.
+		int price;
+		String itemName;
+		boolean isExit = false;
+		
+		while(true) {
+			
+			switch(choice) {
+				
+				case 1:
+					System.out.println("\nInstructions : press below keys for operation");
+					System.out.println("1. To get Instructions.");
+					System.out.println("2. Add item to the stock.");
+					System.out.println("3. Remove item from stock.");
+					System.out.println("4. Display Stock Items.");
+					System.out.println("Enter any other number for exit.\n");
+					break;
+				
+				case 2:
+					itemName = getItemName();
+					price = getPrice();
+					ShoppingCart.addStockItem(itemName, price);
+					break;
+				
+				case 3:
+					itemName = getItemName();
+					ShoppingCart.discardStockItem(itemName);
+					break;
+					
+				case 4:
+					displayStockItems();
+					break;
+				
+				default:
+					isExit = true;
+			}
+			
+			if (isExit) {
+				System.out.println("Thanks for your time.");
+				break;
+			}
+			
+			System.out.println("Enter your choice : ");
+			choice = getIntegerValueOnly();
+		}
+	}
+	
+	/*
+	 * Main method uses helper methods to provide complete functionality.
+	 */
+	public static void main(String args[]) {
+		String person;
+		
+		System.out.println("Enter 'user' or 'admin' : ");
+		person = sc.next();
+		person = person.toLowerCase();
+		
+		if ("user".equals(person)) {
+			
+			// Functionality for user.
+			displayStockItems();
+			userActions();
+			
+		} else if("admin".equals(person)) {
+			
+			// Functionality for administrator.
+			displayStockItems();
+			adminActions();
+			
+		} else {
+			
 			System.out.println("You do not have permission to access this.");
 		}
 		sc.close();
 	}
-		
 }
--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,33 +1,41 @@
 import java.util.Map;
 import java.util.HashMap;
-public class ShoppingCart
-{
-	private static final Map<String, Integer> availableItems = new HashMap<String, Integer>(); //Items with their prices available in store.
-	private Map<String, Integer> cartItems; //Cart for the user to put items and quantities.
-	// Initializing the Menu of Items.
-	static 
-	{
-		//Putting item and price per quantity.
+
+public class ShoppingCart {
+	/* Items with their prices available in store. */
+	private static final Map<String, Integer> availableItems = new HashMap<String, Integer>(); 
+	/* Cart of the user to put items and quantities. */
+	private Map<String, Integer> cartItems; 
+	
+	/* Initializing the Menu of Items. */
+	static {
+		/* Putting item and price per quantity. */
 		availableItems.put("notebook", 50);
 		availableItems.put("pen", 20);
 		availableItems.put("pencil", 10);
 		availableItems.put("crayons", 40);
 		availableItems.put("rubber", 10);
 	}
-	public ShoppingCart()
-	{
-		// cartItems contain the item and corresponding quantity.
+	
+	/*
+	 * It creates an empty cart. 
+	 */
+	public ShoppingCart() {
+		
 		cartItems = new HashMap<String, Integer>();
 	}
+	
 	/*
-	 * Display available items to user.
+	 * Display available items in the stock to user.
 	 */
-	public static void displayAvailableItems()
-	{
+	public static void displayAvailableItems() {
+		
 		System.out.println("Available Items in store");
-		for (String item : availableItems.keySet())
-		{
+		
+		/* Get each item from cart and print item and its price */
+		for (String item : availableItems.keySet()) {
 			int pricePerQuantity = availableItems.get(item);
+			
 			System.out.println("Item Name : " + item + ", Price(for one quantity) : " + pricePerQuantity);
 		}
 	}
@@ -38,31 +46,32 @@ public class ShoppingCart
 	 * @param itemName : item to be added.
 	 * @param quantity : quantity of the item. Assume quantity > 0;
 	 */
-	public void addItem(String itemName, int quantity)
-	{
+	public void addItem(String itemName, int quantity) {
 		itemName = itemName.toLowerCase();
-		if (quantity > 0)
-		{
-			if (availableItems.containsKey(itemName)) // Validation Check : Checks whether that item is available in store.
-			{
-				if (cartItems.containsKey(itemName)) // Checks whether item is already available in store.
-				{
+		
+		if (quantity > 0) {
+			
+			/* Validation Check : Checks whether that item is available in store. */
+			if (availableItems.containsKey(itemName)) {
+				
+				/* Validation Check : Checks whether item is already available in user's cart. */
+				if (cartItems.containsKey(itemName)) {
 					final int previousQuantity = cartItems.get(itemName);
 					final int newQuantity = previousQuantity + quantity;
+					
 					cartItems.put(itemName, newQuantity);
-				}
-				else
-				{
+				} else {
+					
 					cartItems.put(itemName, quantity);
 				}
-			}
-			else
-			{
+
+			} else {
+				
 				System.out.println(itemName + " is not available in store.");
 			}
-		}
-		else
-		{
+
+		} else {
+			
 			System.out.println("Quantity should be greater than 0.");
 		}
 	}
@@ -72,24 +81,27 @@ public class ShoppingCart
 	 * @param itemName : item to be updated. 
 	 * @param quantity : quantity to be updated.
 	 */
-	public void updateItem(String itemName, int quantity)
-	{
+	public void updateItem(String itemName, int quantity) {
 		itemName = itemName.toLowerCase();
+		
 		if (quantity == 0) {
+			
 			removeItem(itemName);
-		}
-		else if (quantity > 0) {
-			//Validation Check : Checks whether that item is available in the cart already.
+		} else if (quantity > 0) {
+			
+			/*Validation Check : Checks whether that 
+			  item is available in the user's cart already. */
 			if (cartItems.containsKey(itemName)) {
+				
 				cartItems.replace(itemName, quantity); // Updates quantity.
 				System.out.println(itemName + " updated to quantity " + quantity);
-			}
-			else {
+			} else {
+				
 				System.out.println(itemName + " is not available in Cart.");
 			}
-		}
-		else
-		{
+
+		} else {
+			
 			System.out.println("quantity should be greater than 0.");
 		}
 	}
@@ -98,16 +110,16 @@ public class ShoppingCart
 	 * Remove item from the cart if available.
 	 * @param itemName : item to be removed.
 	 */
-	public void removeItem(String itemName)
-	{
+	public void removeItem(String itemName) {
 		itemName = itemName.toLowerCase();
-		if (cartItems.containsKey(itemName)) // Checks whether item is present in the cart.
-		{
+		
+		/* Checks whether item is present in the cart. */
+		if (cartItems.containsKey(itemName)) {
+			
 			cartItems.remove(itemName);
 			System.out.println(itemName + " removed from cart.");
-		}
-		else
-		{
+		} else {
+			
 			System.out.println(itemName + " is not present in the cart.");
 		}
 	}
@@ -115,40 +127,39 @@ public class ShoppingCart
 	/*
 	 * Display Cart Items.
 	 */
-	public void displayCart()
-	{
-		if (!cartItems.isEmpty())
-		{
-			for (String item : cartItems.keySet())
-			{
+	public void displayCart() {
+		
+		if (!cartItems.isEmpty()) {
+			
+			for (String item : cartItems.keySet()) {
 				System.out.println("Item : " + item + ", Quantity : " + cartItems.get(item));
 			}
-		}
-		else
-		{
+			
+		} else {
 			System.out.println("Cart is Empty.");
 		}
 	}
+	
 	/*
-	 * Generate Bill.
+	 * Generates Bill.
 	 */
-	public void generateBill()
-	{
-		if (!cartItems.isEmpty())
-		{
+	public void generateBill() {
+		
+		if (!cartItems.isEmpty()) {
 			int grandTotal = 0;
-			for (String item : cartItems.keySet())
-			{
-				int quantity = cartItems.get(item); // Gives quantity of the item.
-				int priceOfEachItem = availableItems.get(item); // Gives price of the item.
+			
+			for (String item : cartItems.keySet()) {
+				int quantity = cartItems.get(item); 
+				int priceOfEachItem = availableItems.get(item); 
 				int totalPrice = quantity * priceOfEachItem;
+				
 				System.out.println("Item : " + item + ", Price(for one quantity) : " + priceOfEachItem + ", Quantity : " + quantity + ", Total Price : " + totalPrice);
 				grandTotal += totalPrice;
 			}
+			
 			System.out.println("Grand Total : " + grandTotal);
-		}
-		else
-		{
+		} else {
+			
 			System.out.println("Cart is Empty.");
 		}
 	}
@@ -158,23 +169,25 @@ public class ShoppingCart
 	 * @param itemName : item to added to store.
 	 * @param price : price of the item.
 	 */
-	public static void addStockItem(String itemName, int price)
-	{
+	public static void addStockItem(String itemName, int price) {
+		itemName = itemName.toLowerCase();
+		
 		availableItems.put(itemName, price);
 	}
+	
 	/*
 	 * Remove items from store.
 	 * @param itemName : item to be removed from store.
 	 */
-	public static void discardStockItem(String itemName)
-	{
-		if (availableItems.containsKey(itemName))
-		{
+	public static void discardStockItem(String itemName) {
+		itemName = itemName.toLowerCase();
+		
+		if (availableItems.containsKey(itemName)) {
+			
 			availableItems.remove(itemName);
 			System.out.println(itemName + " removed from store.");
-		}
-		else
-		{
+		} else {
+			
 			System.out.println(itemName + " is not available in stock");
 		}
 	}
